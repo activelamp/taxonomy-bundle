0c5ce43c3615f8df5723d782601d76bcb640be69
Bez Hermoso <bez@activelamp.com>
2014-06-16 11:48:21 -0700

    Removed awareness of serialization.

--- src/ActiveLAMP/Bundle/TaxonomyBundle/ALTaxonomyBundle.php
+++ src/ActiveLAMP/Bundle/TaxonomyBundle/ALTaxonomyBundle.php
@@ -8,9 +8,5 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class ALTaxonomyBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
-    {
-        parent::build($container);
-        //$container->addCompilerPass(new SerializationPass());
-    }
+
 }
